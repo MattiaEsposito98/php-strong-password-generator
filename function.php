e302c4abbd3e0582b2bc17f1751715817544896b
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-02-26 13:31:55 +0100

    add filtri

--- function.php
+++ function.php
@@ -1,6 +1,29 @@
 <?php
-function genera($lunghezza)
+function genera($lunghezza, $lettere = "on", $numeri = "on", $simboli = "on")
 {
-  $caratteri = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+';
-  return  substr(str_shuffle(str_repeat($caratteri, ceil($lunghezza / strlen($caratteri)))), 0, $lunghezza);
+  // Inizializza la stringa dei caratteri vuota
+  $caratteri = '';
+
+  // Aggiungi le lettere se richieste
+  if ($lettere === "on") {
+    $caratteri .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  }
+
+  // Aggiungi i numeri se richiesti
+  if ($numeri === "on") {
+    $caratteri .= '0123456789';
+  }
+
+  // Aggiungi i simboli se richiesti
+  if ($simboli === "on") {
+    $caratteri .= '!@#$%^&*()-_=+';
+  }
+
+  // Se non ci sono caratteri disponibili, restituisci una stringa vuota
+  if (empty($caratteri)) {
+    return 'Inserisci un filtro';
+  }
+
+  // Genera la password
+  return substr(str_shuffle(str_repeat($caratteri, ceil($lunghezza / strlen($caratteri)))), 0, $lunghezza);
 }
--- index.php
+++ index.php
@@ -13,6 +13,23 @@
   <form action="" method="get">
     <label for="lunghezza">Quanto deve essere lunga la password (minimo 5 e massimo 20)</label>
     <input type="number" id="lunghezza" name="lunghezza" min="5" max="20">
+
+    <label for="">Seleziona i filtri</label>
+    <div>
+      <label for="">Lettere</label>
+      <input type="checkbox" name="lettere" id="lettere">
+    </div>
+
+    <div>
+      <label for="">Numeri</label>
+      <input type="checkbox" name="numeri" id="numeri">
+    </div>
+
+    <div>
+      <label for="">Simboli</label>
+      <input type="checkbox" name="simboli" id="simboli">
+    </div>
+
     <button type="submit">Invia</button>
     <button type="reset">Resetta</button>
   </form>
@@ -23,7 +40,11 @@
 
   if (isset($_GET['lunghezza']) && $_GET['lunghezza'] >= 5 && $_GET['lunghezza'] <= 20) {
     $lunghezza = (int)$_GET['lunghezza'];
-    $_SESSION['password'] = genera($lunghezza);
+    $lettere = $_GET['lettere'];
+    $numeri = $_GET['numeri'];
+    $simboli = $_GET['simboli'];
+
+    $_SESSION['password'] = genera($lunghezza, $lettere, $numeri, $simboli);
     header('Location: ./results.php');
   }
 
