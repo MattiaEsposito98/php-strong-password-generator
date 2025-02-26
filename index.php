a516bddfe2600ed68744f214d1386bde29cc6517
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-02-26 12:32:36 +0100

    creato un file per la funzione

--- index.php
+++ index.php
@@ -18,18 +18,7 @@
 
   <?php
 
-  if (!empty($_GET['lunghezza']) && $_GET['lunghezza'] > 5) {
-    $lunghezza = (int)$_GET['lunghezza'];
-    function genera($lunghezza)
-    {
-      $caratteri = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+';
-      return substr(str_shuffle($caratteri), 0, $lunghezza);
-    }
-    echo genera($lunghezza);
-  }
-  // else {
-  //   echo "inserisci un numero";
-  // }
+  include "./function.php"
   ?>
 
 </body>
